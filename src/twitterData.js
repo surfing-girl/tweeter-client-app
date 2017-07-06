6c4d17033a144e473656b5bff0bb6bb5437769e0
surfing-girl <joanna.demecka@gmail.com>
2017-07-06 12:36:18 +0100

    Refactoring.

--- src/twitterData.js
+++ src/twitterData.js
@@ -4,13 +4,7 @@ const config = require('./config.js');
 
 //setting up authentication
 function TwitterData() {
-  this.T = new Twit({
-    consumer_key:         config.consumer_key,
-    consumer_secret:      config.consumer_secret,
-    access_token:         config.access_token,
-    access_token_secret:  config.access_token_secret,
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  });
+  this.T = new Twit(config);
   this.followersData = [];
   this.authUserData = [];
   this.friendsDirectMsg = [];
